f3c9cd3ced5597bb60e8f4b22277a21e47735df9
EmmahMusenyah <88723139+EmmahMusenyah@users.noreply.github.com>
2021-08-27 14:13:04 +0300

    Added the search engine

--- app.js
+++ app.js
@@ -25,28 +25,41 @@ function displayTemperature(response) {
   console.log(response.data);
 
   let degreesElement = document.querySelector("#degrees");
-  degreesElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
   let comparisonElement = document.querySelector("#comparison");
   let comparison = Math.round(response.data.main.feels_like);
-  comparisonElement.innerHTML = `Feels like ${comparison}°C`;
   let pressureElement = document.querySelector("#pressure");
-  pressureElement.innerHTML = `${response.data.main.pressure}Hg`;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${response.data.main.humidity}%`;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = `${response.data.wind.speed} Km/hr`;
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#icon");
+
+  degreesElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  comparisonElement.innerHTML = `Feels like ${comparison}°C`;
+  pressureElement.innerHTML = `${response.data.main.pressure}Hg`;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} Km/hr`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
-let city = "Nairobi";
-let apiKey = "3ba4ba419c4c391e4a4ac38b121708bc";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3ba4ba419c4c391e4a4ac38b121708bc&units=metric`;
-axios.get(url).then(displayTemperature);
+function search(city) {
+  let apiKey = "3ba4ba419c4c391e4a4ac38b121708bc";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3ba4ba419c4c391e4a4ac38b121708bc&units=metric`;
+  axios.get(url).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  let city = document.querySelector("#city");
+  city.innerHTML = `${cityInputElement.value}`;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
