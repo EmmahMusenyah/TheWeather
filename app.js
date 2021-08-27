04b5d534f71c3e385848539b86bd4ffe0bc46ab2
EmmahMusenyah <88723139+EmmahMusenyah@users.noreply.github.com>
2021-08-27 12:37:20 +0300

    Edited html, and Js
    -Added relevant icon using url
    -Added ids

---
+++ app.js
@@ -0,0 +1,43 @@
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let degreesElement = document.querySelector("#degrees");
+  degreesElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let comparisonElement = document.querySelector("#comparison");
+  let comparison = Math.round(response.data.main.feels_like);
+  comparisonElement.innerHTML = `Feels like ${comparison}°C`;
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = `${response.data.main.pressure}Hg`;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${response.data.wind.speed} Km/hr`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+
+let city = "Nairobi";
+let apiKey = "3ba4ba419c4c391e4a4ac38b121708bc";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3ba4ba419c4c391e4a4ac38b121708bc&units=metric`;
+axios.get(url).then(displayTemperature);
+
+let li = document.querySelector("#current-city");
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekDay = weekDays[now.getDay()];
+li.innerHTML = ` ${weekDay} ${hours}:${minutes}`;
