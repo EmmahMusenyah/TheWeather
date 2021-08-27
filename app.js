ed7ad7f1a2e627699d66f40940ee441d7214a13f
EmmahMusenyah <88723139+EmmahMusenyah@users.noreply.github.com>
2021-08-27 13:19:51 +0300

    Updated the time

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}: ${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data);
 
@@ -14,6 +37,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = `${response.data.wind.speed} Km/hr`;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -25,19 +50,3 @@ let city = "Nairobi";
 let apiKey = "3ba4ba419c4c391e4a4ac38b121708bc";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3ba4ba419c4c391e4a4ac38b121708bc&units=metric`;
 axios.get(url).then(displayTemperature);
-
-let li = document.querySelector("#current-city");
-let now = new Date();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let weekDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let weekDay = weekDays[now.getDay()];
-li.innerHTML = ` ${weekDay} ${hours}:${minutes}`;
