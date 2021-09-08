b8f3afc683d2603f1378c034a7c395a147596428
EmmahMusenyah <88723139+EmmahMusenyah@users.noreply.github.com>
2021-09-08 16:50:47 +0300

    Updated an automatic search city

--- app.js
+++ app.js
@@ -157,3 +157,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Nairobi");
